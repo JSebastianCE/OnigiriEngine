0d98ea7cb059374992be4a70a1e406f447ee6b4d
JSebasti16 <jesbastianc@gmail.com>
2025-05-24 17:34:07 -0600

    Window + CShape

--- OnigiriEngine/OnigiriEngine/src/BaseApp.cpp
+++ OnigiriEngine/OnigiriEngine/src/BaseApp.cpp
@@ -1,8 +1,7 @@
 #include "BaseApp.h"
 
 
-BaseApp::~BaseApp()
-{
+BaseApp::~BaseApp(){
 }
 
 int 
@@ -15,7 +14,7 @@ BaseApp::run()
 	}
 	
 	while (m_window->isOpen()) {
-		handleEvents();
+		m_window->handleEvents();
 		update();
 		render();
 	}
@@ -25,9 +24,8 @@ BaseApp::run()
 }
 
 bool 
-BaseApp::init()
-{
-	m_window = new sf::RenderWindow(sf::VideoMode(800, 600), "Onigiri Engine");
+BaseApp::init(){	
+	m_window = new Window(1920, 1080, "Onigiri Engine");
 	m_circle = new sf::CircleShape(100.0f);
 	m_circle->setFillColor(sf::Color::Yellow);
 	m_circle->setPosition(200.f, 150.f);
@@ -48,20 +46,8 @@ BaseApp::render()
 }
 
 void 
-BaseApp::destroy()
-{
-	delete m_window;
+BaseApp::destroy(){
 	delete m_circle;
+	m_window->destroy();
 }
 
-void 
-BaseApp::handleEvents()
-{
-	sf::Event event;
-	while (m_window->pollEvent(event)) {
-		//Cerrar a ventana si el usuario lo indica
-		if (event.type == sf::Event::Closed) {
-			m_window->close();
-		}
-	}
-}
